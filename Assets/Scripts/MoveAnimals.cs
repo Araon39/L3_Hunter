eadae11902a5d1dbd861e71b6ef9507d8a35a675
Araon39 <115640752+Araon39@users.noreply.github.com>
2024-12-18 20:57:25 +0200

    AddMover
    Добавили движение и физику животным и пули

---
+++ Assets/Scripts/MoveAnimals.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MoveAnimals : MonoBehaviour
+{
+    private int speed = 15; // Переменная для хранения скорости движения
+
+    void Update()
+    {
+        // Перемещаем объект вперед по оси Z с учетом времени и скорости
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
