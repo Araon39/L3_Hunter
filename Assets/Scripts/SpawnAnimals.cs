5798e416a7cdde6c99262dbd524a9f4b0e743381
Araon39 <115640752+Araon39@users.noreply.github.com>
2024-12-20 09:52:50 +0200

    Add Count
    добавил увеличивающийся счетчик животных (сложность игры)

--- Assets/Scripts/SpawnAnimals.cs
+++ Assets/Scripts/SpawnAnimals.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
+//Ётот код создает животных в случайных позици€х через определенные промежутки времени,
+//увеличива€ количество создаваемых животных с каждым вызовом метода Spawn.
+
 public class SpawnAnimals : MonoBehaviour
 {
-    public GameObject[] animals;    // ћассив префабов животных, которые будут по€вл€тьс€
-
-    private int index;              // »ндекс дл€ выбора животного из массива
-    private Vector3 position;       // ѕозици€ дл€ по€влени€ животного
+    
+    public GameObject[] animals;                // ћассив префабов животных, которые будут по€вл€тьс€     
+    private int index;                          // »ндекс дл€ выбора животного из массива    
+    private Vector3 position;                   // ѕозици€ дл€ по€влени€ животного    
+    [SerializeField] private int spawnCount;    //  оличество животных, которые будут по€вл€тьс€
 
     private void Start()
     {
@@ -15,13 +19,19 @@ public class SpawnAnimals : MonoBehaviour
 
     void Spawn()
     {
-        // √енерируем случайную позицию в заданных пределах
-        position = new Vector3(Random.Range(-15, 15), -4.5f, Random.Range(11, 25));
+        for (int i = 0; i < spawnCount; i++)
+        {
+            // √енерируем случайную позицию в заданных пределах
+            position = new Vector3(Random.Range(-15, 15), -4.5f, Random.Range(11, 25));
 
-        // ¬ыбираем случайное животное из массива
-        index = Random.Range(0, animals.Length);
+            // ¬ыбираем случайное животное из массива
+            index = Random.Range(0, animals.Length);
 
-        // —оздаем экземпл€р выбранного животного в случайной позиции с его стандартной ориентацией
-        Instantiate(animals[index], position, animals[index].transform.rotation);
+            // —оздаем экземпл€р выбранного животного в случайной позиции с его стандартной ориентацией
+            Instantiate(animals[index], position, animals[index].transform.rotation);
+        }
+       
+        spawnCount++; // ”величиваем количество животных, которые будут по€вл€тьс€, на 1
     }
 }
+
